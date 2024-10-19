61b107880df0d28bb8a2c40f5dec93639f444a0b
raminour <raminoreldaim@gmail.com>
2024-10-19 11:32:51 +0400

    qr-code-scanner backend setup

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const shortid = require("shortid");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+
+const mongoURI = `mongodb+srv://rami:${process.env.DB_PASSWORD}@qrcode-scanner.scaps.mongodb.net/qr_tracker?retryWrites=true&w=majority&appName=qrcode-scanner`;
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("Error connecting to MongoDB Atlas:", err));
+
+const LinkSchema = new mongoose.Schema({
+  shortCode: String,
+  originalUrl: String,
+  scanCount: { type: Number, default: 0 },
+});
+
+const Link = mongoose.model("Link", LinkSchema);
+
+app.get("/create", async (req, res) => {
+  const { url } = req.query;
+  const shortCode = shortid.generate();
+  const link = new Link({ shortCode, originalUrl: url });
+  await link.save();
+  res.json({ shortCode });
+});
+
+app.get("/:shortCode", async (req, res) => {
+  const { shortCode } = req.params;
+  const link = await Link.findOne({ shortCode });
+  if (link) {
+    link.scanCount++;
+    await link.save();
+    res.redirect(link.originalUrl);
+  } else {
+    res.status(404).send("Not found");
+  }
+});
+
+app.get("/stats/:shortCode", async (req, res) => {
+  const { shortCode } = req.params;
+  const link = await Link.findOne({ shortCode });
+  if (link) {
+    res.json({ scanCount: link.scanCount });
+  } else {
+    res.status(404).send("Not found");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
